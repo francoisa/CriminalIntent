01cd239736538a620bed3172eb7390d17b157f90
francoisa <andre.francois@gmail.com>
2016-10-09 16:54:03 -0400

    End of chapter 7.

--- app/src/main/java/net/jmf/criminalintent/Crime.java
+++ app/src/main/java/net/jmf/criminalintent/Crime.java
@@ -1,8 +1,14 @@
 package net.jmf.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
     public UUID getId() {
         return mId;
     }
@@ -15,10 +21,24 @@ public class Crime {
         return mTitle;
     }
 
-    private UUID mId;
-    private String mTitle;
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 }
--- app/src/main/java/net/jmf/criminalintent/CrimeFragment.java
+++ app/src/main/java/net/jmf/criminalintent/CrimeFragment.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,19 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable s) {
                 // Intentionally left blank
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        DateFormat df = new DateFormat();
+        mDateButton.setText(df.format("E - MMM dd, yyyy", mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
         return v;
